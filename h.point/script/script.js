7e917ced1186c3709d3894a0b1d0281384e6eebf
oliveves <oliveves@gmail.com>
2023-05-30 16:59:41 +0900

    스크립트 완료

--- h.point/script/script.js
+++ h.point/script/script.js
@@ -1,3 +1,29 @@
+
 $(window).on("load", function(){
+    // 로고클릭시 최상단
+    $(".logo").on("click", function(){
+        $("html, body").stop().animate({scrollTop:0}, 400);
+    });
+
+
+    // 하단버튼 클릭시 최상단
+    $(".btn-top").on("click", function(){
+        $("html, body").stop().animate({scrollTop:0}, 400);
+    })
+
+
+    // 하단버튼이 스크롤 500픽셀 도달시 나타남
+    $(".btn-top").hide();
+
+    $(window).scroll(function(){
+        if($(this).scrollTop()>500){
+            $(".btn-top").fadeIn();
+        } else {
+            $(".btn-top").fadeOut();
+        }
+    });
+
+    // 각 메뉴 클릭시 애니메이션으로 각 컨텐츠에 위치 이동
+    
 
 });
